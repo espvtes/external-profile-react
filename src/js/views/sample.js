91321a72ecee8ac086cb3e265dd67d8c4dd542a9
ESPINO <espvtes@gmail.com>
2019-05-24 00:27:53 +0000

    views

---
+++ src/js/views/sample.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Sample extends React.Component {
+	render() {
+		return (
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-12">I am here!!!!</div>
+				</div>
+			</div>
+		);
+	}
+}
