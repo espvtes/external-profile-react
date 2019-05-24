83b741dce4a5db11797e4d1372fd5fb518dd21b1
ESPINO <espvtes@gmail.com>
2019-05-24 21:06:14 +0000

    sections

--- src/js/views/sample.js
+++ src/js/views/sample.js
@@ -11,72 +11,74 @@ export class Sample extends React.Component {
 						{/*<div className="row">
 							<div className="col-12 parallaxSample" />
 						</div>*/}
-						<div className="row parallaxSample">
-							<div className="col-12 col-sm-2">
-								<nav className="list-group text-center vertical-menu">
-									<ul className="rounded">
-										<li className="border-bottom">
-											<a
-												href="#section-top"
-												className="section-top">
-												<img
-													src="https://frostsnow.com/uploads/biography/2016/12/19/xjason-statham.jpg.pagespeed.ic.mFhCmaxibX.jpg"
-													className="img-menu"
-													alt="Avatar"
-												/>
-											</a>
-										</li>
-										<li className="border-bottom">
-											<a href="#section-1">
-												<i className="fas fa-file-invoice fa-3x" />
-											</a>
-										</li>
-										<li className="border-bottom">
-											<a href="#section-2">
-												<i className="fas fa-language fa-3x" />
-											</a>
-										</li>
-										<li className="border-bottom">
-											<a href="#section-3">
-												<i className="fas fa-bicycle fa-3x" />
-											</a>
-										</li>
-										<li className="border-bottom">
-											<a href="#section-4">
-												<i className="fas fa-chart-line fa-3x" />
-											</a>
-										</li>
-										<li className="border-bottom">
-											<a href="#section-5">
-												<i className="far fa-file-code fa-3x" />
-											</a>
-										</li>
-										<li className="border-bottom">
-											<a href="#section-6">
-												<i className="far fa-star fa-3x" />
-											</a>
-										</li>
-										<li className="border-bottom">
-											<a href="#section-8">
-												<i className="fas fa-graduation-cap fa-3x" />
-											</a>
-										</li>
-										<li>
-											<a href="#section-9">
-												<i className="fas fa-user-alt fa-3x" />
-											</a>
-										</li>
-									</ul>
-								</nav>
-							</div>
-							<div className="col-12 col-sm-10">
-								<div className="row d-flex justify-content-end">
-									<div className="col text-right col-img">
-										<img
-											src="https://www.4geeksacademy.co/wp-content/themes/the-fastest/assets/img/4geeks-icon-black.png"
-											className="geeks-img"
-											alt="..."
-										/>
+						<div className="parallaxSample">
+							<div className="row ">
+								<div className="col-12 col-sm-2">
+									<nav className="list-group text-center vertical-menu">
+										<ul className="rounded">
+											<li className="border-bottom">
+												<a
+													href="#section-top"
+													className="section-top">
+													<img
+														src="https://frostsnow.com/uploads/biography/2016/12/19/xjason-statham.jpg.pagespeed.ic.mFhCmaxibX.jpg"
+														className="img-menu"
+														alt="Avatar"
+													/>
+												</a>
+											</li>
+											<li className="border-bottom">
+												<a href="#section-1">
+													<i className="fas fa-file-invoice fa-3x" />
+												</a>
+											</li>
+											<li className="border-bottom">
+												<a href="#section-2">
+													<i className="fas fa-language fa-3x" />
+												</a>
+											</li>
+											<li className="border-bottom">
+												<a href="#section-3">
+													<i className="fas fa-bicycle fa-3x" />
+												</a>
+											</li>
+											<li className="border-bottom">
+												<a href="#section-4">
+													<i className="fas fa-chart-line fa-3x" />
+												</a>
+											</li>
+											<li className="border-bottom">
+												<a href="#section-5">
+													<i className="far fa-file-code fa-3x" />
+												</a>
+											</li>
+											<li className="border-bottom">
+												<a href="#section-6">
+													<i className="far fa-star fa-3x" />
+												</a>
+											</li>
+											<li className="border-bottom">
+												<a href="#section-8">
+													<i className="fas fa-graduation-cap fa-3x" />
+												</a>
+											</li>
+											<li>
+												<a href="#section-9">
+													<i className="fas fa-user-alt fa-3x" />
+												</a>
+											</li>
+										</ul>
+									</nav>
+								</div>
+								<div className="col-12 col-sm-10">
+									<div className="row d-flex justify-content-end">
+										<div className="col text-right col-img">
+											<img
+												src="https://www.4geeksacademy.co/wp-content/themes/the-fastest/assets/img/4geeks-icon-black.png"
+												className="geeks-img"
+												alt="..."
+											/>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -89,6 +91,59 @@ export class Sample extends React.Component {
 								</h1>
 								<br />
 								<h6>Full-Stack Developer</h6>
+							</div>
+						</div>
+						<hr />
+						{/*body*/}
+						<div className="row">
+							<div className="col-12">
+								<div className="container">
+									<div className="row">
+										<div className="col-12 col-md-6">
+											<div className="row">
+												<div className="col-12 bubble text-center font-italic corner-bubble">
+													<label className="font-weight-light">
+														I like saving the world
+													</label>
+												</div>
+											</div>
+											<div className="row">
+												<div className="col-12">
+													<label className="motto font-italic font-weight-light text-muted">
+														Motto
+													</label>
+												</div>
+											</div>
+										</div>
+										<div className="col-12 col-md-6">
+											<div className="row">
+												<div className="col-12 bubble text-center font-italic corner-bubble">
+													<label className="font-weight-light">
+														Killing Bad Boys
+													</label>
+												</div>
+											</div>
+											<div className="row">
+												<div className="col-12">
+													<label className="motto font-italic font-weight-light text-muted">
+														Focus
+													</label>
+												</div>
+											</div>
+										</div>
+									</div>
+									{/*summary*/}
+									<div className="row mb-3 pt-4">
+										<div className="col-12 col-md-1 pr-0">
+											<i className="fas fa-file-invoice text-warning icon" />
+										</div>
+										<div className="col-12 col-md-11 pl-0">
+											<label className="font-italic font-weight-light text-dark title">
+												&nbsp;S&nbsp;u&nbsp;m&nbsp;m&nbsp;a&nbsp;r&nbsp;y
+											</label>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
